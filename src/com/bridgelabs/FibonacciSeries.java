2943b20f7364df82ac59493772d8d3092b77f5a8
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-25 20:44:01 +0530

    Added: Fibonacci Series Program

--- src/com/bridgelabs/FibonacciSeries.java
+++ src/com/bridgelabs/FibonacciSeries.java
@@ -3,8 +3,24 @@ package com.bridgelabs;
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num = 10;
+		series(num);
+	}
+	public static void series(int num) 
+	{
+		int n1 = 0;
+		int n2 = 1;
+		int n3 = 0;
+		System.out.print(n1+" "+n2);
+		for (int i =2; i < num; i++) 
+		{
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+			
+		}
+		
 	}
 
 }
