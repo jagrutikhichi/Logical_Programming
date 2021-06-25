aac26071f023283546676791aec6d85e1ce06cb4
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-25 21:19:56 +0530

    Added: Reverse Number program

---
+++ src/com/bridgelabs/ReverseNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabs;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no. to reverse :");
+		int number = sc.nextInt();
+		int reverse = 0;  
+		while(number != 0)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		number = number/10;  
+		}  
+		System.out.println("The reverse number is: " + reverse); 
+
+	}
+
+}
